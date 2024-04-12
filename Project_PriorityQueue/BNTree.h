815fead47463afec05eeeb158c1bc26094d3d829
HUYENMY <huyenmyp02012003@gmail.com>
2024-04-12 13:14:08 +0700

    Add project files.

---
+++ Project_PriorityQueue/BNTree.h
@@ -0,0 +1,31 @@
+﻿#ifndef BNTree_h
+#define BNTree_h
+//lefter file
+//tên nguyễn trọng giáp
+//lớp THCTDL-buoi-01
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+
+typedef int ItemType;
+
+struct BSTNode {
+	ItemType data; //lưu trữ thông tin
+	BSTNode* Left;
+	BSTNode* Right;
+};
+
+// Định nghĩa kiểu dữ liệu cho dBSTk đơn 
+struct BSTree {
+	BSTNode* Root;
+};
+BSTNode* taoBSTNode(ItemType x);
+void showBSTNode(BSTNode* p);
+void initBSTree(BSTree& BST);
+int isEmpty(BSTree BST);
+void showNLR(BSTNode* Root);
+int insertRoot(BSTNode*& Root, BSTNode* p);
+void taodanhsachtudong(BSTree& BST);
+
+#endif BNTree_h
